b91e88bf31dcca722eaa580343a20bcd8c9d404f
david419kr <david419kr@gmail.com>
2021-03-29 19:48:37 +0900

    refactoring / passport kakao 가입 및 인증시 email 부분 처리에 관한 주석

--- src/passport/kakao.ts
+++ src/passport/kakao.ts
@@ -19,6 +19,11 @@ export default () => {
           } }
         } = profile;
         try {
+          /* 카카오의 경우, email을 받아오려면 고객 동의를 따로 받아야 해서 email을 아예 받아오지 않도록 설계.
+           * 대신 카카오의 고유 숫자ID를 받아와서 DB의 email 칸에 "Kakao 00000" 같은 형식으로 등록하여 그 회원만의 고유 넘버로 구분 가능하도록 했습니다.
+           * 사실 이대로도 구분에 별 문제는 없지만, 만약 나중에 DB를 재설계할 일이 생긴다면,
+           * 아예 DB에 카카오인지 구글인지 네이버인지 소셜로그인 여부를 저장해놓는 칼럼을 한 줄 만들어 놓으면 더 편할 것 같긴 합니다.
+           */
           const users = (await createQueryBuilder("user")
             .where("email = :email", { email: "Kakao " + id })
             .execute());
